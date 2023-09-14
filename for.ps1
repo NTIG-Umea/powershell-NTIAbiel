61e643523727142aa40b7682264c552dae9a0f24
Abiel Woldu <abiel.woldu@elev.ga.ntig.se>
2023-09-14 15:09:53 +0200

    loopar

---
+++ for.ps1
@@ -0,0 +1,5 @@
+#For-loop: repeterar ett bestämt antal gånger.
+
+for ($i = 1; $i -lt 10; $i++) {
+    Write-Output $i
+}
---
+++ foreach.ps1
@@ -0,0 +1,5 @@
+$names = @("Ompa", "Lompa", "Mankey", "Tom")
+
+foreach($name in $names){
+    Write-Output "$name is a ape!"
+}
