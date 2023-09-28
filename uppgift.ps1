bb02175abef7deb02536b6a8c7dfb15c8d4d8c4a
Abiel Woldu <abiel.woldu@elev.ga.ntig.se>
2023-09-28 13:31:31 +0200

    iploop

--- uppgift.ps1
+++ uppgift.ps1
@@ -1,3 +1,6 @@
-for ($i = 0; $i -lt 256; $i++) {
-    Write-Output "192.168.0.$i "
+for ($i = 1; $i -lt 256; $i++) {
+    $ping =  Test-Connection "10.80.44.$i" -Quiet -Count 1
+    if ($ping -eq $true) {
+    Write-Output "10.80.44.$i"
+    }
 }
