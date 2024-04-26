1af02b738cb4cccb7e34dfd2337bc5b34ccfbad3
Abiel Woldu <abiel.woldu@elev.ga.ntig.se>
2024-04-26 13:41:57 +0300

    enkel

--- assignfolder.ps1
+++ assignfolder.ps1
@@ -5,4 +5,4 @@ Add-ADGroupMember -Identity "abiel's cool anvandare" -Members Eva.Olsson
 
 New-ADGroup -Name "Abiel's best anvandare" -SamAccountName ABA -GroupCategory Security -GroupScope Global -DisplayName "Abiel's Best Anvandare" -Path "OU=Abiel's anvandare,DC=walters,DC=labb" -Description "Medlemmar i det har grupp ar best"
 
-#enkel och vanlig
+#enkel
